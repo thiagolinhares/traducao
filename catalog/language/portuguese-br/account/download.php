1ba74469121e29b11656ebdb0ccfde51795a8d87
thiagolinhares <z.linha@gmail.com>
2014-11-04 12:45:19 -0200

    Update download.php
    100%

--- catalog/language/portuguese-br/account/download.php
+++ catalog/language/portuguese-br/account/download.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']     = 'Account Downloads';
+$_['heading_title']     = 'Downloads da conta';
 
 // Text
-$_['text_account']      = 'Account';
+$_['text_account']      = 'Conta';
 $_['text_downloads']    = 'Downloads';
-$_['text_empty']        = 'You have not made any previous downloadable orders!';
+$_['text_empty']        = 'Você ainda não fez download de pedidos!';
 
 // Column
-$_['column_order_id']   = 'Order ID';
-$_['column_name']       = 'Name';
-$_['column_size']       = 'Size';
-$_['column_date_added'] = 'Date Added';
+$_['column_order_id']   = 'ID Pedido';
+$_['column_name']       = 'Nome';
+$_['column_size']       = 'Tamanho';
+$_['column_date_added'] = 'Data inclusão';
