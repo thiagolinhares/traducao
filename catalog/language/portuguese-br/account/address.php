4205bd3502ae6bc43f4af7833991eedfa2223a8d
thiagolinhares <z.linha@gmail.com>
2014-11-04 12:43:22 -0200

    Update address.php
    100%

--- catalog/language/portuguese-br/account/address.php
+++ catalog/language/portuguese-br/account/address.php
@@ -1,37 +1,37 @@
 <?php
 // Heading
-$_['heading_title']        = 'Address Book';
+$_['heading_title']        = 'Lista de endereços';
 
 // Text
-$_['text_account']         = 'Account';
-$_['text_address_book']    = 'Address Book Entries';
-$_['text_edit_address']    = 'Edit Address';
-$_['text_add']             = 'Your address has been successfully inserted';
-$_['text_edit']            = 'Your address has been successfully updated';
-$_['text_delete']          = 'Your address has been successfully deleted';
-$_['text_empty']           = 'You have no addresses in your account.';
+$_['text_account']         = 'Conta';
+$_['text_address_book']    = 'Endereços';
+$_['text_edit_address']    = 'Editar endereço';
+$_['text_add']             = 'Seu endereço foi adicionado com sucesso!';
+$_['text_edit']            = 'Seu endereço foi atualizado com sucesso!';
+$_['text_delete']          = 'Seu endereço foi removido com sucesso!';
+$_['text_empty']           = 'Você não possui endereços em sua conta!';
 
 // Entry
-$_['entry_firstname']      = 'First Name';
-$_['entry_lastname']       = 'Last Name';
-$_['entry_company']        = 'Company';
-$_['entry_address_1']      = 'Address 1';
-$_['entry_address_2']      = 'Address 2';
-$_['entry_postcode']       = 'Post Code';
-$_['entry_city']           = 'City';
-$_['entry_country']        = 'Country';
-$_['entry_zone']           = 'Region / State';
-$_['entry_default']        = 'Default Address';
+$_['entry_firstname']      = 'Nome';
+$_['entry_lastname']       = 'Sobrenome';
+$_['entry_company']        = 'Empresa';
+$_['entry_address_1']      = 'Endereço 1';
+$_['entry_address_2']      = 'Endereço 2';
+$_['entry_postcode']       = 'CEP';
+$_['entry_city']           = 'Cidade';
+$_['entry_country']        = 'País';
+$_['entry_zone']           = 'Estado';
+$_['entry_default']        = 'Endereço padrão';
 
 // Error
-$_['error_delete']         = 'Warning: You must have at least one address!';
-$_['error_default']        = 'Warning: You can not delete your default address!';
-$_['error_firstname']      = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';
-$_['error_vat']            = 'VAT number is invalid!';
-$_['error_address_1']      = 'Address must be between 3 and 128 characters!';
-$_['error_postcode']       = 'Postcode must be between 2 and 10 characters!';
-$_['error_city']           = 'City must be between 2 and 128 characters!';
-$_['error_country']        = 'Please select a country!';
-$_['error_zone']           = 'Please select a region / state!';
-$_['error_custom_field']   = '%s required!';
+$_['error_delete']         = 'Atenção: Você precisa ter ao menos um endereço';
+$_['error_default']        = 'Atenção: Você precisa ter ao menos um endereço padrão';
+$_['error_firstname']      = 'Nome precisa conter de 1 a 32 caracteres!';
+$_['error_lastname']       = 'Sobrenome precisa conter de 1 a 32 caracteres!';
+$_['error_vat']            = 'Número VAT inválido!';
+$_['error_address_1']      = 'Endereço precisa conter de 3 a 128 caracteres!';
+$_['error_postcode']       = 'CEP precisa conter de 3 a 128 caracteres!';
+$_['error_city']           = 'Cidade precisa conter de 3 a 128 caracteres!';
+$_['error_country']        = 'Por favor escolha um País!';
+$_['error_zone']           = 'Por favor escolha um Estado!';
+$_['error_custom_field']   = '%s necessário';
